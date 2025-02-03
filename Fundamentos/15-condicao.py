d271f9357666c3114d4af52d52e35e66ded5b599
Bruno <brunocarv4519@gmail.com>
2025-02-03 16:32:56 -0300

    condições

---
+++ Fundamentos/15-condicao.py
@@ -0,0 +1,20 @@
+num1 = float(input("Digite um número:"))
+num2 = float(input("Digite outro número:"))
+oper = input("Escolha a operação (+ - * /):")
+
+if oper == "+":
+    result = num1 + num1
+elif oper =="-":
+    result = num1 - num2
+elif oper =="*":
+    result = num1 * num2
+elif oper =="/":
+    if num1 != 0:
+        result = num1 / num2
+    else:
+        print("Não é possível divisão por zero")
+else: 
+    print("Operação inválida")
+
+
+print(f"O resultado da operação é {result}")
