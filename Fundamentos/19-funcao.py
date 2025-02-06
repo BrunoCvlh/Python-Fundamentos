d5440e9aebcc8664d407b39b0931a80719c99e68
Bruno Carvalho <brunocarv4519@gmail.com>
2025-02-06 07:23:16 -0300

    funcao

---
+++ Fundamentos/19-funcao.py
@@ -0,0 +1,25 @@
+# 1 - Função para imprimir uma mensagem
+
+def welcome():
+    print("Seja bem vindo!")
+
+welcome()
+
+#for i in range(10):
+#welcome()
+# 2 - Função para calcular a média de notas
+def calculate_average():
+    num_ratings = int(input("Digite quantas notas deseja declarar:"))
+    total = 0
+    for i in range(num_ratings):
+        note = float(input("Digite a nota do filme: "))
+        total += note
+
+    if num_ratings > 0:
+        average = total / num_ratings
+    else:
+        average = 0
+    return average
+
+
+print(f"A média das notas foi {calculate_average()}")
